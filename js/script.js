153e0142815254f209e1fa6271719edb7c48fd7e
ALan Ruz <alanruz245@gmail.com>
2025-08-12 11:57:37 -0400

    v1.2 de E-Commerce

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const listCategories = document.querySelector("#listCategories");
 const containerCards = document.querySelector("#containerCards");
+const btnAll = document.querySelector("#all");
 let products = {};
 let categories = ["All"];
-const urls = 'https://dummyjson.com/products';
-
+const urls = "https://dummyjson.com/products";
+let posicitionCat = null;
 // * Funciones
 
 // * Crear elemento
@@ -12,55 +13,69 @@ const crearElemento = (element) => {
 };
 
 // * Busca la data en la api
-async function buscaData(url) {
+async function buscaData(url, callback) {
   try {
     const result = await fetch(url);
     const data = await result.json();
-    await processData(data);
+    await callback(data);
     // console.log(typeof(data));
-    
   } catch (error) {
     console.error(error);
     alert(error);
-    return
+    return;
   }
 }
 
 // * Procesa la data y busca las categorias para ordenar la información
-async function processData(data) {
+async function processDataCategoria(data) {
   products = Object.entries(data.products);
   let i = 1;
   console.log(products);
   categories = await products.map(([key, product]) => {
     return product.category;
   });
-  
-  categories = new Set(categories);
-  
+
+  // * Set -> Crea un objeto de valores unicos, es decir toma un objeto
+  categories = [...new Set(categories)];
+
   insertCategories(categories);
+}
+
+// * Procesa la data y manda todos los productos
+async function processDataAll(data) {
+  products = Object.entries(data.products);
+  let i = 1;
+
+  products.forEach(([key, product]) => {
+    crearCard(product);
+  });
 }
 
 // * Agg las categorias al dom
 function insertCategories(categories) {
-  categories.forEach((categorie) => {
-    
+  console.log(categories);
+
+  categories.forEach((categorie, index) => {
     const itemCategorie = crearElemento("li");
     itemCategorie.classList.add("item");
     itemCategorie.textContent = categorie;
     listCategories.insertAdjacentElement("beforeend", itemCategorie);
     itemCategorie.addEventListener("click", (e) => {
-      borrarContenido();
-      buscarPorCategoria(e);
+      borrarContenido(posicitionCat);
+      posicitionCat = itemCategorie;
+      itemCategorie.classList.add("select");
+      buscarPorCategoria(e, index);
     });
   });
   //
 }
 
 // * Borramos el contenido de las cards
-function borrarContenido() {
+function borrarContenido(c) {
   const cards = document.querySelectorAll(".card");
 
-  if (cards) {
+  if (cards.length > 0) {
+    c.classList.remove("select");
     cards.forEach((card) => {
       card.remove();
     });
@@ -70,7 +85,7 @@ function borrarContenido() {
 // *  busca por categoria
 function buscarPorCategoria(e) {
   const categoria = e.target.textContent;
-  products.forEach(([key,product]) => {
+  products.forEach(([key, product]) => {
     if (product.category === categoria) {
       crearCard(product);
     } else if (categoria === "All") {
@@ -98,4 +113,19 @@ function crearCard(product) {
   containerCards.insertAdjacentElement("beforeend", card);
 }
 
-buscaData(urls);
+// * Seleccionar todos los elementos
+btnAll.addEventListener("click", (e) => {
+  borrarContenido(posicitionCat);
+  posicitionCat = e.target;
+  e.target.classList.add("select");
+  buscaData(urls, processDataAll);
+});
+
+function iniciarComercio() {
+  posicitionCat = btnAll; 
+  btnAll.classList.add("select");
+  buscaData(urls, processDataAll);
+}
+
+iniciarComercio(); 
+buscaData(urls, processDataCategoria);
